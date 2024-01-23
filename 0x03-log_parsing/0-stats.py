8547de5ab9c5fff511c7373a2479ea1be230e4ce
Oma-yma1 <zgani.omayma@gmail.com>
2024-01-23 12:13:37 -0800

    task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,21 +4,23 @@ import sys
 import signal
 
 
-def print_stats(total_size, status_codes):
+def print_stats(total_size, status_co):
     """function status"""
     print("File size: {}".format(total_size))
-    for code in sorted(status_codes):
-        print("{}: {}".format(code, status_codes[code]))
+    for code in sorted(status_co):
+        print("{}: {}".format(code, status_co[code]))
+
 
 def signal_handler(sig, frame):
-    print_stats(total_size, status_codes)
+    print_stats(total_size, status_co)
     sys.exit(0)
+
 
 signal.signal(signal.SIGINT, signal_handler)
 
 line_count = 0
 total_size = 0
-status_codes = {}
+status_co = {}
 
 try:
     for line in sys.stdin:
@@ -32,13 +34,13 @@ try:
             if status_code.isdigit():
                 status_code = int(status_code)
                 if status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
-                    status_codes[status_code] = status_codes.get(status_code, 0) + 1
+                    status_co[status_code] = status_co.get(status_code, 0) + 1
 
             line_count += 1
 
             if line_count % 10 == 0:
-                print_stats(total_size, status_codes)
+                print_stats(total_size, status_co)
 
 except KeyboardInterrupt:
-    print_stats(total_size, status_codes)
+    print_stats(total_size, status_co)
     sys.exit(0)
