333672febc0d0938b47a0a4b6d9cdb579a91512b
Oma-yma1 <zgani.omayma@gmail.com>
2024-04-18 15:15:06 -0700

    task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,40 +4,20 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+const apiurl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
+request(apiurl, async (error, response, body) => {
+  error && console.log(error);
 
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch data. Status code:', response.statusCode);
-    return;
-  }
+  const charactersArray = (JSON.parse(response.body).characters);
+  for (const character of charactersArray) {
+    await new Promise((resolve, reject) => {
+      request(character, (error, response, body) => {
+        error && console.log(error);
 
-  const movieData = JSON.parse(body);
-  const characters = movieData.characters;
-
-  const printCharacters = (characterUrls) => {
-    characterUrls.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error('Error:', error);
-          return;
-        }
-
-        if (response.statusCode !== 200) {
-          console.error('Failed to fetch character data. Status code:', response.statusCode);
-          return;
-        }
-
-        const characterData = JSON.parse(body);
-        console.log(characterData.name);
+        console.log(JSON.parse(body).name);
+        resolve();
       });
     });
-  };
-
-  printCharacters(characters);
+  }
 });
