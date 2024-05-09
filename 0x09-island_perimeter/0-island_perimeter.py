e706af46b3c67716beea04689fb01fa1ff9af6ab
Oma-yma1 <zgani.omayma@gmail.com>
2024-05-09 08:11:34 -0700

    task

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+island Perimeter project
+"""
+
+
+def island_perimeter(grid):
+    """Island Perimenter Function"""
+    peri = 0
+    for j in range(len(fun)):
+        for k in range(len(fun[j])):
+            if fun[j][k] == 1:
+                peri += 4
+                if j > 0 and fun[j-1][k] == 1:
+                    peri -= 2
+                if k > 0 and fun[j][k-1] == 1:
+                    peri -= 2
+    return peri
