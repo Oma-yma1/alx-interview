fe1e7f5890f6b63a81dc54d51b674a092181e5c2
Oma-yma1 <zgani.omayma@gmail.com>
2024-05-16 15:45:15 -0700

    task

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,57 @@
 #!/usr/bin/python3
 """
-Module Prime numbers
+Prime Game Module
 """
 
-def is_prime(num):
-    """
-    return list of prime numbers
-    """
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
 
-def generate_primes(n):
-    """
-    generate prime
-    """
-    primes = []
-    for i in range(2, n + 1):
-        if is_prime(i):
-            primes.append(i)
-    return primes
+def sieve(n):
+    """Generate a list of primes up to n using Sieve of Eratosthenes"""
+    is_prime = [True] * (n + 1)
+    p = 2
+    while (p * p <= n):
+        if is_prime[p]:
+            for i in range(p * p, n + 1, p):
+                is_prime[i] = False
+        p += 1
+    return [p for p in range(2, n + 1) if is_prime[p]]
+
+
+def calculate_moves(n, primes):
+    """Calculate number of moves possible with given primes"""
+    marked = [False] * (n + 1)
+    moves = 0
+    for prime in primes:
+        if prime > n:
+            break
+        if not marked[prime]:
+            moves += 1
+            for multiple in range(prime, n + 1, prime):
+                marked[multiple] = True
+    return moves
+
 
 def isWinner(x, nums):
     """
-    return name of the player that won the most rounds
+    Determine the winner of the game
     """
+    if not nums or x < 1:
+        return None
+
+    max_num = max(nums)
+    primes = sieve(max_num)
+
     winners = {'Maria': 0, 'Ben': 0}
 
     for n in nums:
-        primes = generate_primes(n)
-        if len(primes) % 2 == 0:
+        moves = calculate_moves(n, primes)
+        if moves % 2 == 0:
             winners['Ben'] += 1
         else:
             winners['Maria'] += 1
 
-    if winners['Maria'] == winners['Ben']:
+    if winners['Maria'] > winners['Ben']:
+        return 'Maria'
+    elif winners['Ben'] > winners['Maria']:
+        return 'Ben'
+    else:
         return None
-    return max(winners, key=winners.get)
-
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
